5a4c825f05d169950746934aa98a49d9db79a4a8
Patrick Nguyen <patar.nguyen@gmail.com>
2022-01-19 15:25:48 -0500

    Snake

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+
+  console.log('connected to server!');
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: Patar');
+    console.log('hello');
+  })
+  
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
